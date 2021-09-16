317475a38e11b138d1efec6637b79e644b1572db
praveencbe <90701548+praveencbe@users.noreply.github.com>
2021-09-16 20:16:05 +0530

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,27 @@
+/*
+ * Author: PraveenKumar A (a.praveenkumar.ece@gmail.com)
+ * Purpose: Hello world program
+ * Language:  C
+ * Created:  16-09-2021
+ */
+  
+#include <stdio.h>
+int main() 
+{
+   char  name[25] ;
+    scanf("%s",name);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT*/
+    printf("Hello, World!\n");
+    printf("%s", name);
+    return 0;
+}
+/* output:
+Praveen
+Hello, World!
+Praveen
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+*/
